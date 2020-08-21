31e185bbee475b11d35a6837456d49cfb858e7a8
Simone Curzi <simonecurzi@hotmail.com>
2020-08-21 17:41:06 +0200

    Fixes for support to .NET Core.

---
+++ Tools/TmtImport/ModelLoader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using ThreatsManager.Engine;
+using ThreatsManager.Interfaces;
+using ThreatsManager.Interfaces.ObjectModel;
+using ThreatsManager.MsTmt;
+using ThreatsManager.Packaging;
+using ThreatsManager.Utilities;
+
+namespace TmtImport
+{
+    class ModelLoader
+    {
+        private List<string> _missingTypes = new List<string>();
+
+        public ModelLoader()
+        {
+            KnownTypesBinder.TypeNotFound += OnTypeNotFound;
+            ExtensionsConfigurationManager.SetConfigurationUserLevel(ConfigurationUserLevel.None);
+            Manager.Instance.LoadExtensions(ExecutionMode.Simplified);
+            Manager.Instance.ApplyExtensionInitializers();
+        }
+
+        private void OnTypeNotFound(string assemblyName, string typeName)
+        {
+#if NETCOREAPP
+            if (assemblyName == "mscorelib")
+                assemblyName = "System.Private.CoreLib";
+#else
+            if (assemblyName == "System.Private.CoreLib")
+                assemblyName = "mscorelib";
+#endif
+
+            if (string.CompareOrdinal(assemblyName, "mscorlib") == 0)
+            {
+                var regex = new Regex(@"\[\[(?<class>[.\w]*), (?<assembly>[.\w]*)\]\]");
+                var match = regex.Match(typeName);
+                if (match.Success)
+                {
+                    assemblyName = match.Groups["assembly"].Value;
+                    typeName = match.Groups["class"].Value;
+                }
+            }
+            else if (string.CompareOrdinal(assemblyName, "System.Private.CoreLib") == 0)
+            {
+                var regex = new Regex(@"\[\[(?<class>[.\w]*), (?<assembly>[.\w]*)\]\]");
+                var match = regex.Match(typeName);
+                if (match.Success)
+                {
+                    assemblyName = match.Groups["assembly"].Value;
+                    typeName = match.Groups["class"].Value;
+                }
+            }
+
+            var name = $"{assemblyName}#{typeName}";
+
+            if (!_missingTypes.Contains(name))
+            {
+                _missingTypes.Add(name);
+                var parts = typeName.Split('.');
+                Console.WriteLine($"Document uses type {parts.Last()} from {assemblyName}, which is unknown.\nThe document will be loaded but some information may be missing.");
+            }
+        }
+
+        public IThreatModel ConvertModel(string fileName)
+        {
+            var importer = new Importer();
+
+            var result = ThreatModelManager.GetDefaultInstance();
+            result.InitializeStandardSeverities();
+            result.InitializeStandardStrengths();
+
+            importer.Import(result, fileName, 1.0f, null, out var diagrams, out var externalInteractors,
+                out var processes, out var dataStores, out var flows, out var trustBoundaries, out var itemTypes,
+                out var threatTypes, out var customThreatTypes, out var threats, out var missingThreats);
+
+            return result;
+        }
+    }
+}
---
+++ Tools/TmtImport/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using ThreatsManager.Engine.Config;
+using ThreatsManager.Interfaces.ObjectModel;
+using ThreatsManager.Interfaces.ObjectModel.Entities;
+using ThreatsManager.Packaging;
+using ThreatsManager.Utilities;
+
+namespace TmtImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (Directory.Exists(args[0]))
+                {
+                    Console.WriteLine("Threat Modeling Program - MS TMT Files Converter");
+
+                    var loader = new ModelLoader();
+
+                    Analyze(loader, args[0]);
+                }
+                else
+                {
+                    throw new FileNotFoundException("Unable to find the specified file.", args[0]);
+                }
+            }
+        }
+
+        static void Analyze(ModelLoader loader, string directory)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"- Analyzing folder {directory}");
+
+            var docs = Directory.GetFiles(directory, "*.tm7")?.ToArray();
+            if (docs?.Any() ?? false)
+            {
+                foreach (var doc in docs)
+                    ConvertModel(loader, doc);
+            }
+
+            var templates = Directory.GetFiles(directory, "*.tb7")?.ToArray();
+            if (templates?.Any() ?? false)
+            {
+                foreach (var template in templates)
+                {
+                    GenerateTemplate(template, ConvertModel(loader, template));
+                }
+            }
+
+            var folders = Directory.GetDirectories(directory)?.ToArray();
+            if (folders?.Any() ?? false)
+            {
+                foreach (var folder in folders)
+                    Analyze(loader, folder);
+            }
+        }
+
+        static IThreatModel ConvertModel(ModelLoader loader, string fileName)
+        {
+            Console.Write($"--- Converting file {Path.GetFileName(fileName)}");
+
+            var model = loader.ConvertModel(fileName);
+
+            if (model != null)
+            {
+                var tmSerialized = ThreatModelManager.Serialize(model);
+                var dest = Path.Combine(Path.GetDirectoryName(fileName), $"{Path.GetFileNameWithoutExtension(fileName)}.tm");
+                var package = Package.Create(dest);
+                package.Add("threatmodel.json", tmSerialized);
+                package.Save();
+            }
+
+            Console.WriteLine(" - done.");
+
+            return model;
+        }
+
+        static void GenerateTemplate(string pathName, IThreatModel model)
+        {
+            var dest = Path.Combine(Path.GetDirectoryName(pathName), $"{Path.GetFileNameWithoutExtension(pathName)}.tmt");
+            model?.SaveTemplate(new DuplicationDefinition()
+            {
+                AllEntityTemplates = true,
+                AllFlowTemplates = true,
+                AllMitigations = true,
+                AllProperties = true,
+                AllPropertySchemas = true,
+                AllSeverities = true,
+                AllStrengths = true,
+                AllThreatTypes = true,
+                AllTrustBoundaryTemplates = true
+            }, model.Name ?? Path.GetFileNameWithoutExtension(pathName), model.Description, dest);
+
+            Console.WriteLine($"--- Generated template {Path.GetFileName(dest)}");
+        }
+    }
+}
