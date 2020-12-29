aed847c2952fe74d75967811409b398fa4aefe1c
Simone Curzi <simonecurzi@hotmail.com>
2020-12-29 17:53:54 +0100

    Annotations

--- Sources/Extensions/ThreatsManager.Quality/Initializers/QualityInitializer.cs
+++ Sources/Extensions/ThreatsManager.Quality/Initializers/QualityInitializer.cs
@@ -1,17 +1,21 @@
 ﻿using ThreatsManager.Interfaces;
 using ThreatsManager.Interfaces.Extensions;
+using ThreatsManager.Quality.Annotations;
 using ThreatsManager.Quality.Schemas;
 using ThreatsManager.Utilities;
 
 namespace ThreatsManager.Quality.Initializers
 {
-    [Extension("1069F3AA-BD5E-41C6-B681-0D37FCA12937", "Quality Initializer", 10, ExecutionMode.Simplified)]
+    [Extension("1069F3AA-BD5E-41C6-B681-0D37FCA12937", "Quality Initializer", 10, ExecutionMode.Business)]
     public class QualityInitializer : IExtensionInitializer
     {
         public void Initialize()
         {
             KnownTypesBinder.AddKnownType(typeof(FalsePositiveList));
             KnownTypesBinder.AddKnownType(typeof(FalsePositiveInfo));
+            KnownTypesBinder.AddKnownType(typeof(Annotation));
+            KnownTypesBinder.AddKnownType(typeof(AnnotationAnswer));
+            KnownTypesBinder.AddKnownType(typeof(TopicToBeClarified));
         }
     }
 }
