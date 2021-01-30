53388940cf67599f864d8613671e01626eea8404
Simone Curzi <simonecurzi@hotmail.com>
2021-01-30 14:12:59 +0100

    Roadmap Providers

---
+++ Sources/Extensions/ThreatsManager.Extensions.Client/IRoadmapPropertyProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using ThreatsManager.Interfaces;
+using ThreatsManager.Interfaces.Extensions;
+using ThreatsManager.Interfaces.ObjectModel.ThreatsMitigations;
+
+namespace ThreatsManager.Extensions
+{
+    /// <summary>
+    /// Property Provider used to provide properties for the items in the Roadmap Panel.
+    /// </summary>
+    [ExtensionDescription("Roadmap Property Provider")]
+    public interface IRoadmapPropertyProvider : IExtension
+    {
+        /// <summary>
+        /// Name of the Property.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Value of the Property, calculated for the Mitigation.
+        /// </summary>
+        /// <returns>Value of the Property.</returns>
+        string GetValue(IMitigation mitigation);
+    }
+}
