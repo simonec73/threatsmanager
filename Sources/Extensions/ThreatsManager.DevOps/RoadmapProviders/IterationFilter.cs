723a493ee03ee06505ad387aa4108e2ae41ae336
Simone Curzi <simonecurzi@hotmail.com>
2021-02-02 21:10:55 +0100

    Roadmap Filter Providers

---
+++ Sources/Extensions/ThreatsManager.DevOps/RoadmapProviders/IterationFilter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PostSharp.Patterns.Contracts;
+using ThreatsManager.DevOps.Schemas;
+using ThreatsManager.Extensions;
+using ThreatsManager.Interfaces.ObjectModel.ThreatsMitigations;
+
+namespace ThreatsManager.DevOps.RoadmapProviders
+{
+    public class IterationFilter : IRoadmapFilter
+    {
+        private readonly Iteration _iteration;
+
+        public IterationFilter([NotNull] Iteration iteration)
+        {
+            _iteration = iteration;
+        }
+
+        public string Name => $"Show only Mitigations belonging to {_iteration.Name}";
+
+        public IEnumerable<IMitigation> Filter(IEnumerable<IMitigation> mitigations)
+        {
+            IEnumerable<IMitigation> result = null;
+
+            var items = mitigations?.ToArray();
+            if (items?.Any() ?? false)
+            {
+                var model = items.First().Model;
+                if (model != null)
+                {
+                    var schemaManager = new DevOpsPropertySchemaManager(model);
+
+                    var list = new List<IMitigation>();
+                    foreach (var item in items)
+                    {
+                        if (string.CompareOrdinal(schemaManager.GetFirstSeenOn(item)?.IterationId, _iteration.Id) == 0)
+                        {
+                            list.Add(item);
+                        }
+                    }
+
+                    if (list.Any())
+                        result = list.AsReadOnly();
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Sources/Extensions/ThreatsManager.Extensions.Client/IRoadmapFilterProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ThreatsManager.Interfaces;
+using ThreatsManager.Interfaces.Extensions;
+using ThreatsManager.Interfaces.ObjectModel;
+
+namespace ThreatsManager.Extensions
+{
+    /// <summary>
+    /// Get filters for the Roadmap.
+    /// </summary>
+    [ExtensionDescription("Roadmap Filter Provider")]
+    public interface IRoadmapFilterProvider : IExtension
+    {
+        /// <summary>
+        /// Returns the filters defined by the Provider for the Threat Model.
+        /// </summary>
+        /// <param name="model">Threat Model for which the filters must be calculated.</param>
+        /// <returns>Enumerations of the available Roadmap Filters.</returns>
+        IEnumerable<IRoadmapFilter> GetFilters(IThreatModel model);
+    }
+}
