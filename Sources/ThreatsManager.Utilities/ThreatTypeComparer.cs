c8e9008e5cffb045740965281b645ee1f091a472
Simone Curzi <simonecurzi@hotmail.com>
2021-05-24 10:02:13 +0200

    Moved from WinForms

---
+++ Sources/ThreatsManager.Utilities/ThreatTypeComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ThreatsManager.Interfaces.ObjectModel.ThreatsMitigations;
+
+namespace ThreatsManager.Utilities
+{
+    public class ThreatTypeComparer : IEqualityComparer<IThreatType>
+    {
+        public bool Equals(IThreatType x, IThreatType y)
+        {
+            return (x == null && y == null) || (x != null && y != null && x.Id == y.Id);
+        }
+
+        public int GetHashCode(IThreatType obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
