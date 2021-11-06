e807a7a657f11e4426c04347c8ccd95fb8f347ba
Simone Curzi <simonecurzi@hotmail.com>
2021-11-06 11:57:55 +0100

    Cleanup

--- Sources/ThreatsManager.Engine/ObjectModel/ThreatModel.PropertySchemas.cs
+++ Sources/ThreatsManager.Engine/ObjectModel/ThreatModel.PropertySchemas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using PostSharp.Patterns.Contracts;
 using ThreatsManager.Engine.ObjectModel.Properties;
