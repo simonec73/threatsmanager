c5a2f4761bc3569e5b5eb5fdfa3eda3b9ff1e527
Simone Curzi <simonecurzi@hotmail.com>
2023-08-19 12:11:24 +0200

    Added BugBar placeholders

---
+++ Sources/Extensions/ThreatsManager.Extensions/Reporting/CounterBugBarCriticalPlaceholder.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Linq;
+using ThreatsManager.Extensions.Schemas;
+using ThreatsManager.Interfaces;
+using ThreatsManager.Interfaces.ObjectModel;
+
+namespace ThreatsManager.Extensions.Reporting
+{
+    [Extension("E6A884C5-42C8-4060-B9CB-3015238E21B9", "Bug Bar Residual Risk Estimator Critical Threats Placeholder", 100, ExecutionMode.Business)]
+    public class CounterBugBarCriticalPlaceholderFactory : IPlaceholderFactory
+    {
+        public string Qualifier => "CounterBugBarCritical";
+
+        public IPlaceholder Create(string parameters = null)
+        {
+            return new CounterBugBarCriticalPlaceholder();
+        }
+    }
+
+    public class CounterBugBarCriticalPlaceholder : ICounterPlaceholder
+    {
+        public string Name => "BugBarCritical";
+        public string Label => "Bug Bar Risk Estimator Critical Threats parameter";
+        public PlaceholderSection Section => PlaceholderSection.Counter;
+        public Bitmap Image => null;
+
+        public int GetCounter(IThreatModel model)
+        {
+            var schemaManager = new ResidualRiskEstimatorPropertySchemaManager(model);
+            var configured = schemaManager?.Parameters?.ToArray();
+
+            var value = configured?
+               .FirstOrDefault(x => x.Name.Contains("Critical"))?
+               .Value ?? 0.0f;
+            if (value < 0.0f)
+                value = 100.0f;
+
+            return (int)value;
+        }
+    }
+}
---
+++ Sources/Extensions/ThreatsManager.Extensions/Reporting/CounterBugBarLowPlaceholder.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Linq;
+using ThreatsManager.Extensions.Schemas;
+using ThreatsManager.Interfaces;
+using ThreatsManager.Interfaces.ObjectModel;
+
+namespace ThreatsManager.Extensions.Reporting
+{
+    [Extension("3551913E-C074-4E29-AABF-F13BBC341151", "Bug Bar Residual Risk Estimator Low Threats Placeholder", 103, ExecutionMode.Business)]
+    public class CounterBugBarLowPlaceholderFactory : IPlaceholderFactory
+    {
+        public string Qualifier => "CounterBugBarLow";
+
+        public IPlaceholder Create(string parameters = null)
+        {
+            return new CounterBugBarLowPlaceholder();
+        }
+    }
+
+    public class CounterBugBarLowPlaceholder : ICounterPlaceholder
+    {
+        public string Name => "BugBarLow";
+        public string Label => "Bug Bar Risk Estimator Low Threats parameter";
+        public PlaceholderSection Section => PlaceholderSection.Counter;
+        public Bitmap Image => null;
+
+        public int GetCounter(IThreatModel model)
+        {
+            var schemaManager = new ResidualRiskEstimatorPropertySchemaManager(model);
+            var configured = schemaManager?.Parameters?.ToArray();
+
+            var value = configured?
+               .FirstOrDefault(x => x.Name.Contains("Low"))?
+               .Value ?? 0.0f;
+            if (value < 0.0f)
+                value = 100.0f;
+
+            return (int)value;
+        }
+    }
+}
