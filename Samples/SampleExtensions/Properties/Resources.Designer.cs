a5ed06333f3f2abebcfb9c76692b6532827625b3
Simone Curzi <simonecurzi@hotmail.com>
2023-10-25 17:10:05 +0200

    Update samples to show how to create rules by code

--- Samples/SampleExtensions/Properties/Resources.Designer.cs
+++ Samples/SampleExtensions/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace SampleExtensions.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Robot {
+            get {
+                object obj = ResourceManager.GetObject("Robot", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
